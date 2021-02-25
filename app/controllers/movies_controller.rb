a4fe074ff9c467a595eb9cd58c803366d939849d
EC2 Default User <ec2-user@ip-172-31-45-52.us-east-2.compute.internal>
2021-02-25 06:42:47 +0000

    fixed logic

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -9,6 +9,11 @@ class MoviesController < ApplicationController
   def index
     @all_ratings = Movie.all_ratings
     
+    # set cookie
+      session[:set] = 1
+      session[:saved_ratings] = params[:ratings]
+      session[:saved_sort] = params[:sort]
+    
     # set settings from cookie
     if !params.key?("ratings") and !params.key?("sort") and session[:set] == 1
       params[:ratings] = session[:saved_ratings]
@@ -22,21 +27,12 @@ class MoviesController < ApplicationController
     else
       @ratings_to_show = params[:ratings].keys
       @movies = Movie.where(rating: @ratings_to_show)
-      # set cookie
-      session[:set] = 1
-      session[:saved_ratings] = params[:ratings]
     end
     @movies = @movies.order(params[:sort])
     if params[:sort] == "title"
       @title_color = "hilite bg-warning"
-      # set cookie
-      session[:set] = 1
-      session[:saved_sort] = params[:sort]
     elsif params[:sort] == "release_date"
       @release_color = "hilite bg-warning"
-      # set cookie
-      session[:set] = 1
-      session[:saved_sort] = params[:sort]
     end
   end
 
