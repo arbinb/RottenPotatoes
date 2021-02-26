7bacb5a2847775a443607bc656c4af3cf4c9285b
EC2 Default User <ec2-user@ip-172-31-45-52.us-east-2.compute.internal>
2021-02-26 03:37:35 +0000

    goinna have to do it the ugly way

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -10,7 +10,7 @@ class MoviesController < ApplicationController
     @all_ratings = Movie.all_ratings
     
     # set settings from cookie
-    if params[:ratings].nil? and params[:sort].nil? and params[:home].nil? and session[:set] == 1
+    if params[:ratings].nil? and params[:sort].nil? and !params[:usecookie].nil? and session[:set] == 1
       params[:ratings] = session[:saved_ratings]
       params[:sort] = session[:saved_sort]
       redirect_to(movies_path(params))
